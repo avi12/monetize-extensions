920ff5cb16e909869153b793b4ebf9f672c64f37
avi12 <avi6106@gmail.com>
2022-07-27 01:40:31 +0300

    Fixed typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const fflate = require('fflate');
 const yargs = require('yargs');
-const { argv } = yargs(pricess.argv[1]);
+const { argv } = yargs(process.argv[1]);
 const fs = require('fs');
 
 const data = fflate.unzipSync(fs.createReadStream(argv.zipName));
