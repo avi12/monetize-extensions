2defec83b91ac1124ededf1f4cd28e9a317c4550
avi12 <avi6106@gmail.com>
2022-07-27 00:19:00 +0300

    Created simple script

---
+++ index.js
@@ -0,0 +1,7 @@
+const fflate = require('fflate');
+const yargs = require('yargs');
+const { argv } = yargs(pricess.argv[1]);
+const fs = require('fs');
+
+const data = fflate.unzipSync(fs.createReadStream(argv.zipName));
+console.log(data);
