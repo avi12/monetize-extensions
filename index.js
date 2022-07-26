763bb77dfb4044137d5311e68e25fb75dd91c83d
avi12 <avi6106@gmail.com>
2022-07-27 01:52:15 +0300

    Fixed reading zip

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const yargs = require('yargs');
 const { argv } = yargs(process.argv.slice(2));
 const fs = require('fs');
 
-const data = fflate.unzipSync(fs.createReadStream(argv.zipName));
+const data = fflate.unzipSync(fs.readFileSync(argv.zipName));
 console.log(data);
