5a9333fc3ba57bdd6230333c4dccbf072109fe29
avi12 <avi6106@gmail.com>
2022-07-27 01:43:02 +0300

    Fixed yargs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const fflate = require('fflate');
 const yargs = require('yargs');
-const { argv } = yargs(process.argv[1]);
+const { argv } = yargs(process.argv.slice(2));
 const fs = require('fs');
 
 const data = fflate.unzipSync(fs.createReadStream(argv.zipName));
