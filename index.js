ba5b8cf79c78c3d6a0046c80ae84313f208f4622
avi12 <avi6106@gmail.com>
2022-07-27 11:42:44 +0300

    Logging manifest input

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const manifestData = JSON.parse(new TextDecoder().decode(zipData['manifest.json'
 const manifestInput = JSON.parse(
   fs.readFileSync(argv.manifestFilenameInput).toString()
 );
+console.log(manifestInput);
 
 for (const key in manifestInput) {
   if (Array.isArray(manifestData[key])) {
