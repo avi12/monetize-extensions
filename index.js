9de25f54050d442ddcd1c5ad704c1dea51d385b0
avi12 <avi6106@gmail.com>
2022-07-27 10:15:07 +0300

    Creating a new ZIP

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const fflate = require('fflate');
 const yargs = require('yargs');
 const { argv } = yargs(process.argv.slice(2));
 const fs = require('fs');
+const getStorage = require('key-file-getStorage');
 
-const data = fflate.unzipSync(fs.readFileSync(argv.zipName));
-console.log(data);
+const zipData = fflate.unzipSync(fs.readFileSync(argv.zipName), {
+  filter: (file) => file.name === 'manifest.json',
+});
+
+const manifestData = JSON.parse(data['manifest.json'].toString());
+const manifestInput = JSON.parse(
+  fs.readFileSync(argv.manifestFilenameInput).toString()
+);
+
+for (const key in manifestInput) {
+  manifestData[key] = { ...manifestData[key], ...manifestInput[key] };
+}
+
+const zip = fflate.zipSync({
+  ...zipData,
+  monetization: getStorage(argv.pathMonetization),
+});
+
+const zipName = argv.zipName
+  .replace('{version}', manifestData.version)
+  .replace('.zip', '-chrome.zip');
+fs.writeFileSync(zipName, zip);
+
+console.log('Created', zipName);
